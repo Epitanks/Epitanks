7197aad572cb1d9cd45133b32f839ee0f9bcc906
Etienne PASSOT <etienne.passot@epitech.eu>
2018-03-26 10:46:24 +0200

    [*] move with keydown

--- move.py
+++ move.py
@@ -1,28 +1,60 @@
-import pygame
+import pygame, sys, time
 from pygame.locals import *
 
 pygame.init()
 
-fenetre = pygame.display.set_mode((640, 480))
+FPS=30
+fpsClock=pygame.time.Clock()
 
-fond = pygame.image.load("Assets/background_example.png").convert()
-fenetre.blit(fond, (0,0))
+width=400
+height=300
+DISPLAYSURF=pygame.display.set_mode((width,height),0,32)
+pygame.display.set_caption('Animation')
+background=pygame.image.load('Assets/background_example.png')
 
-perso = pygame.image.load("Assets/PNG/tank_dark.png").convert_alpha()
-perso_x = 0
-perso_y = 0
-fenetre.blit(perso, (perso_x, perso_y))
 
-pygame.display.flip()
+UP='up'
+LEFT='left'
+RIGHT='right'
+DOWN='down'
 
-continuer = 1
-while continuer:
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			continuer = 0
-		if event.type == KEYDOWN and event.key == K_UP:
-				perso_x += 10
-	
-	fenetre.blit(fond, (0,0))	
-	fenetre.blit(perso, (perso_x, perso_y))
-	pygame.display.flip()
+sprite=pygame.image.load('Assets/PNG/tank_blue.png')
+spritex=200
+spritey=130
+direction=None
+
+def move(direction, sprite, spritex, spritey):
+    if direction:
+        if direction == K_UP:
+            spritey-=5
+            sprite=pygame.image.load('Assets/PNG/tank_blue.png')
+        elif direction == K_DOWN:
+            spritey+=5
+            sprite=pygame.image.load('Assets/PNG/tank_dark.png')
+        if direction == K_LEFT:
+            spritex-=5
+            sprite=pygame.image.load('Assets/PNG/tank_green.png')
+        elif direction == K_RIGHT:
+            spritex+=5
+            sprite=pygame.image.load('Assets/PNG/tank_red.png')
+    return sprite, spritex, spritey
+
+while True:
+    DISPLAYSURF.blit(background,(0,0))
+
+    DISPLAYSURF.blit(sprite,(spritex,spritey))
+
+    for event in pygame.event.get():
+        if event.type==QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == KEYDOWN:
+            direction = event.key
+        if event.type == KEYUP:
+            if (event.key == direction):
+                direction = None
+    sprite, spritex, spritey = move(direction, sprite, spritex, spritey)
+
+    pygame.display.update()
+    fpsClock.tick(FPS)
